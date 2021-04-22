c817ffc31675ba2f48f15efb3993780eeb18109b
JMuriloSS <jmprg07@gmail.com>
2021-04-21 23:02:00 -0300

    Seção 07/aula 99/React TodoApp(Frontend)-Componentes Grid e IconButtom

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
+import Grid from "../template/grid";
+import IconButtom from "../template/iconButtom"
 
 export default (props) => (
 	<div role="form" className="todoForm row">
-		<div className="col-xs-12 col-sm-9 col-md-10">
-			<input id="description" className="form-control"
-			placeholder="Adicione uma tarefa"/>
-		</div>
-		<div className="col-xs-12 col-sm-3 col-md-2" >
-			<button className="btn btn-primary">
-				<i className="fa fa-plus "></i>
-			</button>
-		</div>
+		<Grid cols="12 9 10">
+			<input
+				id="description"
+				className="form-control"
+				placeholder="Adicione uma tarefa"
+			/>
+		</Grid>
+
+		<Grid cols="12 3 2">
+			<IconButtom style="primary" icon="plus"></IconButtom>
+		</Grid>
 	</div>
 );
